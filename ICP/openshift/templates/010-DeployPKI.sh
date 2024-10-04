9c6b7dffca97c26797e7dccc35017751614ec254
Julio Cesar Torres dos Santos <julio.cesartorres@mcn.gouv.qc.ca>
2024-10-04 12:59:39 -0400

    [func] supprime lignes commentées
    Signed-off-by: Julio Cesar Torres dos Santos <julio.cesartorres@mcn.gouv.qc.ca>

--- ICP/openshift/templates/010-DeployPKI.sh
+++ ICP/openshift/templates/010-DeployPKI.sh
@@ -1,5 +1,1 @@
-# oc process -f ../../../aries-toolkit/openshift/templates/issuer/aries-issuer.yaml --param-file=agent/aries-issuer.candy-dev.params.env | oc apply -f -
-
-# oc process -f ../../../aries-toolkit/openshift/templates/issuer/aries-issuer.yaml --param-file=agent/aries-issuer.candy-dev.params.env 
-
 oc process -f ./pki-cqen.yaml | oc apply -f -
